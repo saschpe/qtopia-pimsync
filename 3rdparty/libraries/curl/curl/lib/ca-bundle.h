be2942dd9b733d259c10988f32ca17713edd47b6
Sascha Peilicke <speilicke@cato>
2008-04-21 16:27:34 +0200

    curl config fixes

--- 3rdparty/libraries/curl/curl/lib/ca-bundle.h
+++ 3rdparty/libraries/curl/curl/lib/ca-bundle.h
@@ -1,3 +1,1 @@
-/* This file is generated automatically */
-#define CURL_CA_BUNDLE "/etc/ssl/certs/ca-certificates.crt"
-#undef CURL_CA_PATH /* unknown default path */
+/* ca bundle path set in here*/
